285cb6d70effa008bb7f6206c923de535d198b92
MarcCalvetCasas <98348722+MarcCalvetCasas@users.noreply.github.com>
2022-01-24 19:56:35 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace DisHub
     {
         static void Main(string[] args)
         {
+            int tuNuevaHermana, yo, tuMadre;
             Console.WriteLine("La cosa aquesta ara si funca tu");
             Console.WriteLine("Ara a veure que tal desde la vaina aquesta");
+            
+            tuNuevaHermana = yo + tuMadre;
+           
             //A
         }
     }
