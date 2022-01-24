b11c2b7ba1ce26dea7272fcf9286d2941811546c
GiliusASe <98348779+GiliusASe@users.noreply.github.com>
2022-01-24 19:36:58 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ namespace DisHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("La cosa aquesta ara si funca tu");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("La cosa aquesta no funca tu");
         }
     }
 }
