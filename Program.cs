3dadb0d7b88246b275045cb76a738622dae40f85
MarcCalvetCasas <98348722+MarcCalvetCasas@users.noreply.github.com>
2022-01-24 20:00:58 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,13 +6,7 @@ namespace DisHub
     {
         static void Main(string[] args)
         {
-            int tuNuevaHermana, yo, tuMadre;
-            Console.WriteLine("La cosa aquesta ara si funca tu");
-            Console.WriteLine("Ara a veure que tal desde la vaina aquesta");
-                       
-            tuNuevaHermana = yo + tuMadre;
-           
-            //A
+            Console.WriteLine("ESCRIBE AQUI PARA DECLARAR TU HOMOSEXUALIDAD (EXSESPSION DE EL CREADOR DE LA BRANCH (VAYA PALABRA DE MIERDA")
         }
     }
 }
