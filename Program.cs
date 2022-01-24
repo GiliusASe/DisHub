22a19f2bd5f64b4dd4813b914a37da533151f2eb
AlbertAP <98348794+AlbertAP@users.noreply.github.com>
2022-01-24 20:07:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,16 +6,7 @@ namespace DisHub
     {
         static void Main(string[] args)
         {
-            int tuNuevaHermana, yo, tuMadre;
-            Console.WriteLine("La cosa aquesta ara si funca tu");
-            Console.WriteLine("Ara a veure que tal desde la vaina aquesta");
-                       
-            tuNuevaHermana = yo + tuMadre;
-            
-            Console.WriteLine("EL CREADOR DE ESTA RAMA ES GAY");
-           Console.WriteLine("bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas bebés en bolas ");
-            
-            //insertar a funny "tu tío" joke
+            Console.WriteLine("*chiste*");
         }
     }
 }
