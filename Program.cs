f1f01bd46cc834786aacfdd2aa864bb4e070b55e
Gil <a2106874@institutmontilivi.cat>
2022-01-24 19:29:17 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DisHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
