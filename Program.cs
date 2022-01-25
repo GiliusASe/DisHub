10b9d8d4852ab93ef1b6af17dd06c8b0c6cab2eb
GiliusASe <98348779+GiliusASe@users.noreply.github.com>
2022-01-25 15:29:45 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,11 +6,10 @@ namespace DisHub
     {
         static void Main(string[] args)
         {
-            int tuNuevaHermana, yo, tuMadre;
-                     
-            tuNuevaHermana = yo + tuMadre;
-           
-            //LA MASTER NO ESTA PARA MAMADAS MIJO
+            int coronaviru;
+            Console.WriteLine("Roblox Corporation was founded by David Baszucki and Erik Cassel. Baszucki had previously founded Knowledge Revolution, an educational software company, in 1989. That year, through the company, he and Cassel developed Interactive Physics, a 2D physics simulation.[3][4] Knowledge Revolution followed this up with Working Model, which simulated mechanical devices.[3] The company was eventually purchased in December 1998 for US$20 million by MSC Software, where Baszucki and Cassel obtained senior positions.[3][5] Baszucki was the company's vice president and general manager from 2000 until 2002, when he left MSC Software to establish Baszucki & Associates, an angel investment firm.[5] He and Cassel founded Roblox Corporation in 2004.[4][5] Working from an office in Menlo Park, California, they began preliminary work on the video game DynaBlocks, which was launched in a beta state later that year.[3][5] The game's name was changed to Roblox in 2005, and the game was formally released on September 1, 2006.");
+            coronaviru = convert.ToInt32(Console.Readline());
+            //A
         }
     }
 }
