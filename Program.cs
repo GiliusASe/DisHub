10661fb1d4f3d79aa720fce58024d44b2f3a3e87
GiliusASe <98348779+GiliusASe@users.noreply.github.com>
2022-01-25 15:19:16 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace DisHub
         static void Main(string[] args)
         {
             Console.WriteLine("ESCRIBE AQUI PARA DECLARAR TU HOMOSEXUALIDAD (EXSESPSION DE EL CREADOR DE LA BRANCH (VAYA PALABRA DE MIERDA");
+            Console.WriteLine("Si soy");
         }
     }
 }
