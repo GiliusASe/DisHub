2cbf0303ca58657e2c057b2223e4e02a21c3ca5e
MarcCalvetCasas <98348722+MarcCalvetCasas@users.noreply.github.com>
2022-02-07 19:35:55 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DisHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("El señor dueño de esta rama ha admitido ser fanático empedernido del miembro reproductor masculino");
+            
         }
     }
 }
