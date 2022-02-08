57e62a91d752a64cce47f893ea8b73fd017e56c2
AlbertAP <98348794+AlbertAP@users.noreply.github.com>
2022-02-08 15:29:44 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,5 +8,20 @@ namespace DisHub
         {
             
         }
+        public static void DoInformatica()
+        {
+            string resposta1, resposta2, resposta3, resposta4, resposta5;
+            Console.WriteLine("Qina es la part fisica d'una computadora ?");
+            resposta1 = Console.ReadLine();
+            Console.WriteLine("Quina es la part logica d'una computadora ?");
+            resposta2 = Console.ReadLine();
+            Console.WriteLine("Que volen dir les sigles de RAM ?");
+            resposta3 = Console.ReadLine();
+            Console.WriteLine("Que indica la mascara de xarxa ?");
+            resposta4 = Console.ReadLine();
+            Console.WriteLine("Com funciona el subneting ?");
+            resposta5 = Console.ReadLine();
+            DoClear();
+        }
     }
 }
