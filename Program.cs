ac5ca1da654e09eb1c28280ddef46bfff3c51c81
DavidHinojosaMont <98348852+DavidHinojosaMont@users.noreply.github.com>
2022-02-08 15:27:20 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace DisHub
     {
         static void Main(string[] args)
         {
-           
+          
         }
+        public static void MostrarOpcions()
+        {
+            Console.WriteLine("---ESCULL EL TEMA PER RESPONDRE LES PREGUNTES---)
+            Console.WriteLine("1- HISTORIA");
+            Console.WriteLine("2- GEOGRAFIA");
+            Console.WriteLine("3- ART");
     }
 }
