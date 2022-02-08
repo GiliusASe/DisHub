ef9e58690161071f3b72d80a1be470ab402c8ff7
DavidHinojosaMont <98348852+DavidHinojosaMont@users.noreply.github.com>
2022-02-08 15:26:09 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,59 +23,33 @@ namespace DisHub
                     //break es per acabar la comanda del case
                     case ConsoleKey.D1:
                     case ConsoleKey.NumPad1:
-                        DoAnyTraspas();
+                        DoHistoria();
                         MissatgeSeguentPantalla("PREM UNA TECLA PER TORNAR AL MENÚ");
                         break;
                     case ConsoleKey.D2:
                     case ConsoleKey.NumPad2:
-                        DoMCD();
+                        DoGeografia();
                         MissatgeSeguentPantalla("PREM UNA TECLA PER TORNAR AL MENÚ");
                         break;
                     case ConsoleKey.D3:
                     case ConsoleKey.NumPad3:
-                        DoMCM() ;
+                        DoArt() ;
                         MissatgeSeguentPantalla("PREM UNA TECLA PER TORNAR AL MENU");
                         break;
-                    case ConsoleKey.D4:
-                    case ConsoleKey.NumPad4:
-                        DoValidarData();
-                        MissatgeSeguentPantalla("PREM UNA TECLA PER TORNAR AL MENU");
-                        break;
-                    case ConsoleKey.D5:
-                    case ConsoleKey.NumPad5:
-                        DoNumeroPrimer();
-                        break;
-                    case ConsoleKey.D6:
-                    case ConsoleKey.NumPad6:
-                        MostrarOpcions2();
-                        break;
+
+
+
         }
         public static void MostrarOpcions()
         {
-            Console.WriteLine("1- ANY DE TRASPAS");
-            Console.WriteLine("2- CALCULAR MCD");
-            Console.WriteLine("3- CALCULAR MCM");
-            Console.WriteLine("4- VALIDAR DATA");
-            Console.WriteLine("5- SABER SI # ES PRIMER")
-            Console.WriteLine("0- EXIT");
+            Console.WriteLine("---ESCULL EL TEMA PER RESPONDRE LES PREGUNTES---)
+            Console.WriteLine("1- HISTORIA");
+            Console.WriteLine("2- GEOGRAFIA");
+            Console.WriteLine("3- ART");
+
         }
-        public static void MostrarOpcions2()
-        {
-            Console.WriteLine("1- ANY DE TRASPAS");
-            Console.WriteLine("2- CALCULAR MCD");
-            Console.WriteLine("3- CALCULAR MCM");
-            Console.WriteLine("4- VALIDAR DATA");
-            Console.WriteLine("5- SABER SI # ES PRIMER")
-            Console.WriteLine("0- EXIT");
-        }
-        public static void MostrarOpcions3()
-        {
-            Console.WriteLine("1- ANY DE TRASPAS");
-            Console.WriteLine("2- CALCULAR MCD");
-            Console.WriteLine("3- CALCULAR MCM");
-            Console.WriteLine("4- VALIDAR DATA");
-            Console.WriteLine("5- SABER SI # ES PRIMER")
-            Console.WriteLine("0- EXIT");
-        }
+        public static void DoHistoria()
+                              {}
+                                 
     }
 }
