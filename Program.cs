e3b105a9c631242986dede04386c6601263903b5
MarcCalvetCasas <98348722+MarcCalvetCasas@users.noreply.github.com>
2022-02-13 18:35:27 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -90,5 +90,85 @@ namespace DisHub
             Console.ReadKey();
             Console.Clear();
         }
+        
+        public static void DoAnime()
+        {
+            char resposta;
+
+            // Pregunta 1
+            Console.WriteLine("Qui es el Protagonista de Kimetsu no Yaiba?");
+            Console.WriteLine("A) Gonpanchiro Kamaboko");
+            Console.WriteLine("B) Itadaki Tontaro");
+            Console.WriteLine("C) Inosuke Hashibira");
+            Console.WriteLine("D) Monjiro Masado");
+            resposta = Convert.ToChar(Console.ReadLine());
+
+            if (resposta == 'C') Console.WriteLine("Resposa Correcta");
+            else Console.WriteLine("Resposta Incorrecta");
+
+            Console.WriteLine("CLIC ALGUNA TECLA PER PASSAR DE PREGUNTA");
+            Console.ReadKey();
+            Console.Clear();
+
+            // Pregunta 2
+            Console.WriteLine("Quantes partes te JoJo's Bizzare Adventure");
+            Console.WriteLine("A) 9");
+            Console.WriteLine("B) 8");
+            Console.WriteLine("C) 6");
+            Console.WriteLine("D) 5");
+            resposta = Convert.ToChar(Console.ReadLine());
+
+            if (resposta == 'B') Console.WriteLine("Resposa Correcta");
+            else Console.WriteLine("Resposta Incorrecta");
+
+            Console.WriteLine("CLIC ALGUNA TECLA PER PASSAR DE PREGUNTA");
+            Console.ReadKey();
+            Console.Clear();
+
+            // Pregunta 3
+            Console.WriteLine("Quin estudi es el creador de la obra Koe no Katachi");
+            Console.WriteLine("A) Clover Works");
+            Console.WriteLine("B) MAPPA");
+            Console.WriteLine("C) Ufotable");
+            Console.WriteLine("D) Kyoto Animation");
+            resposta = Convert.ToChar(Console.ReadLine());
+
+            if (resposta == 'D') Console.WriteLine("Resposa Correcta");
+            else Console.WriteLine("Resposta Incorrecta");
+
+            Console.WriteLine("CLIC ALGUNA TECLA PER PASSAR DE PREGUNTA");
+            Console.ReadKey();
+            Console.Clear();
+
+            // Pregunta 4
+            Console.WriteLine("Quin instrument toca el personatge Tsumugi Kotobuki");
+            Console.WriteLine("A) Teclat");
+            Console.WriteLine("B) Guitarra");
+            Console.WriteLine("C) Baix");
+            Console.WriteLine("D) Bateria");
+            resposta = Convert.ToChar(Console.ReadLine());
+
+            if (resposta == 'A') Console.WriteLine("Resposa Correcta");
+            else Console.WriteLine("Resposta Incorrecta");
+
+            Console.WriteLine("CLIC ALGUNA TECLA PER PASSAR DE PREGUNTA");
+            Console.ReadKey();
+            Console.Clear();
+
+            // Pregunta 5
+            Console.WriteLine("Qui es el protagonista de principal de Haikyuu!!");
+            Console.WriteLine("A) Hinata Shouyo");
+            Console.WriteLine("B) Yuu Nishinoya");
+            Console.WriteLine("C) Daichi Sawamura");
+            Console.WriteLine("D) Cap es Correcta");
+            resposta = Convert.ToChar(Console.ReadLine());
+
+            if (resposta == 'A') Console.WriteLine("Resposa Correcta");
+            else Console.WriteLine("Resposta Incorrecta");
+
+            Console.WriteLine("CLIC ALGUNA TECLA PER PASSAR DE PREGUNTA");
+            Console.ReadKey();
+            Console.Clear();
+        }
     }
 }
