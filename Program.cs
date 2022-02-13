e412eb5c6264c10ef1527d9e3249577e0bdc6768
AlbertAP <98348794+AlbertAP@users.noreply.github.com>
2022-02-13 23:24:59 +0100

    agregacio de temes
    agregacio dels temes le l'alex al menu

--- Program.cs
+++ Program.cs
@@ -103,6 +103,9 @@ namespace DisHub
             Console.Writeline("7- Musica");
             Console.Writeline("8- Anime");
             Console.Writeline("9- Videojocs");
+            Console.Writeline("9- Fisica");
+            Console.Writeline("9- Quimica");
+            Console.Writeline("9- Biologia");
         }
 
         public static void DoClear()
